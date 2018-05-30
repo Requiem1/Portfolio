d07395174ae06a54ca8faebd1adf9285eac46017
윤혁순 <sdwww123@naver.com>
2018-05-30 12:26:27 +0900

    수정됀 시맨틱에 맞게 어플리케이션 내용 수정

--- Application.cpp
+++ Application.cpp
@@ -14,37 +14,37 @@ Application::~Application()
 
 void Application::Init()
 {
-	g_pDeviceMGR->Init();
-	g_pCamera->Init();
-	g_pINPUTMGR->Init();
+	g_DeviceMGR->Init();
+	g_INPUTMGR->Init();
+	g_Camera->Init();
 }
 
 void Application::Destroy()
 {
 	g_pTextureMGR->Destroy();
-	g_pDeviceMGR->Destroy();
+	g_DeviceMGR->Destroy();
 }
 
 void Application::Update()
 {
-	g_pCamera->Update();
-	//g_pINPUTMGR->Update();
+	//g_INPUTMGR->Update();
+	g_Camera->Update();
 }
 
 void Application::Render()
 {
-	g_pDevice->Clear(NULL, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER,
+	g_Device->Clear(NULL, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER,
 		D3DCOLOR_XRGB(50, 50, 50), 1.0f, 0);
 
 
 
-	g_pDevice->EndScene();
-	g_pDevice->Present(NULL, NULL, NULL, NULL);
+	g_Device->EndScene();
+	g_Device->Present(NULL, NULL, NULL, NULL);
 
 
 }
 
 void Application::WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	g_pCamera->WndProc(hWnd, message, wParam, lParam);
+	g_Camera->WndProc(hWnd, message, wParam, lParam);
 }
