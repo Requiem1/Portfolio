c07b8f925463f10233337691c878a31cfcfc7fd9
DESKTOP-3754E7S\강동현 <gtod8010@naver.com>
2018-06-18 19:50:15 +0900

    0618 Update Zombie animate test

--- Enemy/Zombie.h
+++ Enemy/Zombie.h
@@ -1,34 +1,27 @@
 #pragma once
 class Player;
-
+#include "../Base/IAnimationObject.h"
 enum FSM_STATE
 {
 	STATE_MOVE,
 	STATE_ATTACK
 };
-class Zombie : public IDisplayObject
+class Zombie : public IAnimationObject
 {
 private:
-	vector<VERTEX_PC>		m_vecVertex;
-	vector<WORD>			m_vecIndex;
-	LPDIRECT3DVERTEXBUFFER9 m_pVB;
-	LPDIRECT3DINDEXBUFFER9	m_pIB;
-	D3DVERTEXBUFFER_DESC	m_VBDesc;
-	D3DINDEXBUFFER_DESC		m_IBDesc;
+	D3DXVECTOR3				m_vPlayerPos;
+	D3DXVECTOR3				m_vLookatPlayer;
+	D3DXVECTOR3				m_vForward;
+	D3DXVECTOR3				m_vUp;
+	D3DXVECTOR3				m_vRight;		
 
-	D3DXVECTOR3				m_Destination;
 
 	float					m_moveSpeed;
-	vector<Player*>			m_pPlayer;
+	float					m_rotSpeed;
+	Player*					m_pPlayer;
 	int						m_state;
-
+	bool					m_bAngleCheck;
 	vector<D3DXVECTOR3> vecPos;
-
-	D3DCOLOR red = D3DCOLOR_XRGB(255, 0, 0);
-	D3DCOLOR green = D3DCOLOR_XRGB(0, 255, 0);
-	D3DCOLOR blue = D3DCOLOR_XRGB(0, 0, 255);
-	D3DCOLOR white = D3DCOLOR_XRGB(255, 255, 255);
-	D3DCOLOR yellow = D3DCOLOR_XRGB(255, 255, 0);
 
 public:
 	Zombie();
@@ -37,15 +30,7 @@ public:
 	void Init();
 	void Update();
 	void Render();
+	//void SetPlayer();
 
 
-	void CreateBuffer(LPDIRECT3DVERTEXBUFFER9 & pVb, LPDIRECT3DINDEXBUFFER9 & pIb,
-		vector<VERTEX_PC>& vecVertex, vector<WORD>& vecIndex);
-
-	void SetVertex(vector<VERTEX_PC> &vecVertexOut,
-		vector<WORD> &vecIndexOut, vector<D3DXVECTOR3> vecPos, D3DCOLOR color);
-
-	void SetBuffer(LPDIRECT3DVERTEXBUFFER9 &pVb,
-		LPDIRECT3DINDEXBUFFER9 &pIb,
-		vector<VERTEX_PC> &vecVertex, vector<WORD> &vecIndex);
 };
